2aa9cfb272f8a12b48eac9ee187bbaf61dd7bc97
Andrea c. Baron <andrea.martensc@gmail.com>
2022-09-15 19:09:15 -0300

    variaves  e tipos

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// Tipos primitivos
+// variaveis no javascript
+// operadores matematicos
+
+// serve para riderizar mensagem no terminal
+// string e texto fica entre ""
+console.log("hello word")
+console.log(5)
+console.log(true)
+console.log(false)
+
+// variaveis
+let mansagem = "Minha mansagens"
+console.log(mansagem)
